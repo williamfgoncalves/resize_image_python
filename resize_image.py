522b088e0f9b74169f277191883b5d349b0c063f
William Goncalves <william.goncalves@cwi.com.br>
2020-07-16 23:37:55 -0300

    add variable resize

--- resize_image.py
+++ resize_image.py
@@ -5,14 +5,23 @@ import os
 
 def main():
 
-	IN_PATH = os.path.join(os.getcwd(), "/in_img")
-	OUT_PATH = os.path.join(os.getcwd(), "/out_img")
+	IN_PATH = location("\in_img")
+	OUT_PATH = location("\out_img")
 
 	for image_name in os.listdir(IN_PATH):
 		im = cv2.imread(os.path.join(IN_PATH, image_name))
-		im_resize = cv2.resize(im, (256,256))
+		print(im.shape)
+		im_resize = cv2.resize(im, (1024, resize(1024, im.shape[0], im.shape[1])))
 		cv2.imwrite(os.path.join(OUT_PATH, image_name), im_resize)
 	
+
+def location(path):
+	dirname = os.path.abspath(os.getcwd())
+	return dirname + path
+
+def resize(a, b, c):
+	return (a * b) / c 
+
 if __name__ == "__main__":
 	main()
 
