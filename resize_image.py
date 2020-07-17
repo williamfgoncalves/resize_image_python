be1016ee6ec1f211d5e2dad5553593d078b46d9a
William Goncalves <william.goncalves@cwi.com.br>
2020-07-16 23:39:15 -0300

    cleaning code

--- resize_image.py
+++ resize_image.py
@@ -10,7 +10,6 @@ def main():
 
 	for image_name in os.listdir(IN_PATH):
 		im = cv2.imread(os.path.join(IN_PATH, image_name))
-		print(im.shape)
 		im_resize = cv2.resize(im, (1024, resize(1024, im.shape[0], im.shape[1])))
 		cv2.imwrite(os.path.join(OUT_PATH, image_name), im_resize)
 	
@@ -24,9 +23,3 @@ def resize(a, b, c):
 
 if __name__ == "__main__":
 	main()
-
-# im_shape[0]
-# im_shape[0]
-# im_shape[0]
-	
-#fixar largura em 1024 e altura varia de acordo com a nova largura 
